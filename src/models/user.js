78f08e5af22ead1f59740f6ed8cfa8b99974e10d
panhaleap <leappanha1@yahoo.com>
2018-07-12 21:20:00 +0700

    already bcrypt password success

--- src/models/user.js
+++ src/models/user.js
@@ -52,70 +52,92 @@ export const Users = sequelize.define('user', {
       key: 'id'
     }
   }
+},{
+  hooks:{
+    afterValidate: user => {
+      user.password = bcrypt.hashSync(user.password, 8);
+    }
+  }
 });
 
-Users.associate = function (users) {
-  generateHash(password) {
-    return bcrypt.hash(password, bcrypt.genSaltSync(8));
-},
-validPassword(password) {
-    return bcrypt.compare(password, this.password);
-}
-};
 
 
 
-  instanceMethods: {
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Users.associate = function (users) {
+//   generateHash(password) {
+//     return bcrypt.hash(password, bcrypt.genSaltSync(8));
+// },
+// validPassword(password) {
+//     return bcrypt.compare(password, this.password);
+// }
+// };
+
+
+
+//   instanceMethods: {
       
-  }
+//   }
 
 
 
 
 
 
-Users.pre('save', async function(next) {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(this.password, salt);
+// Users.pre('save', async function(next) {
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     const passwordHash = await bcrypt.hash(this.password, salt);
 
-    console.log('salt', salt);
-    console.log('normal password', this.password);
-    console.log('hashed password', passwordHash);
-  } catch (error) {
-    next();
-  }
-});
-//
+//     console.log('salt', salt);
+//     console.log('normal password', this.password);
+//     console.log('hashed password', passwordHash);
+//   } catch (error) {
+//     next();
+//   }
+// });
+// //
 
-module.exports = function(sequelize, DataTypes) {
-  const User = sequelize.define('users', {
-      annotation_id: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true
-      },
-      firstName: {
-          type: DataTypes.DATE,
-          field: 'first_name'
-      },
-      lastName: {
-          type: DataTypes.DATE,
-          field: 'last_name'
-      },
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
-  }, {
-      freezeTableName: true,
-      instanceMethods: {
-          generateHash(password) {
-              return bcrypt.hash(password, bcrypt.genSaltSync(8));
-          },
-          validPassword(password) {
-              return bcrypt.compare(password, this.password);
-          }
-      }
-  });
+// module.exports = function(sequelize, DataTypes) {
+//   const User = sequelize.define('users', {
+//       annotation_id: {
+//           type: DataTypes.INTEGER,
+//           autoIncrement: true,
+//           primaryKey: true
+//       },
+//       firstName: {
+//           type: DataTypes.DATE,
+//           field: 'first_name'
+//       },
+//       lastName: {
+//           type: DataTypes.DATE,
+//           field: 'last_name'
+//       },
+//       email: DataTypes.STRING,
+//       password: DataTypes.STRING
+//   }, {
+//       freezeTableName: true,
+//       instanceMethods: {
+//           generateHash(password) {
+//               return bcrypt.hash(password, bcrypt.genSaltSync(8));
+//           },
+//           validPassword(password) {
+//               return bcrypt.compare(password, this.password);
+//           }
+//       }
+//   });
 
-  return User;
-}
+//   return User;
+// }
